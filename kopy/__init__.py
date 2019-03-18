0642831232c76093a5aeacfa0dcdc5c2fa0ca685
Martin Trigaux <me@mart-e.be>
2019-03-18 13:12:50 +0100

    [FIX] do not hardcode path
    and add a random secret
    should probably be generated at setup later

--- kopy/__init__.py
+++ kopy/__init__.py
@@ -7,10 +7,12 @@ from .base import StatusManager
 
 def create_app():
     app = Flask(__name__)
+    # TODO retrieve from config/autogenerate
+    app.secret_key = b'\xd6|fp\x12\xf1\xf2\x12=M~\x1c\xbar\xb8\x0e\xdd\x12\xa36Dp\xa2\x00'
 
     manager = StatusManager()
 
-    with open('/home/mart/programmation/kopy/config.json') as f:
+    with open('config.json') as f:
         config = json.load(f)
 
     for name, site_config in config.items():
